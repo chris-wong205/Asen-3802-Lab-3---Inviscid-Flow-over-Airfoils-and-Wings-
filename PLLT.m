4fffb763dfa5120151cee4a69c04a42928a7a18c
chris-wong205 <chwo6932@colorado.edu>
2025-12-01 17:08:16 -0700

    Add files via upload

--- PLLT.m
+++ PLLT.m
@@ -1,4 +1,23 @@
+%% This function is the PLLT Function. 
+%{
+The function includes e the span efficiency factor (to be computed and returned), C_L is the coefficient of lift (to be
+computed and returned), C_Di is the induced coefficient of drag (to be computed and reported), b is the
+span (in feet), a0_t is the cross-sectional lift slope at the tips (per radian), a0_r is the cross-sectional lift
+slope at the root (per radian), c_t is the chord at the tips (in feet), c_r is the chord at the root (in feet),
+aero_t is the zero-lift angle of attack at the tips (in radians), aero_r is the zero-lift angle of attack at the
+root (in radians), geo_t is the geometric angle of attack (geometric twist + α) at the tips (in radians),
+geo_r is the geometric angle of attack (geometric twist + α) at the root (in radians), and N is the number
+of odd terms to include in the series expansion for circulation.
+
+%}
+
+% This code was written in collaboration with Aiden Hayes,Ignacio Huber,
+% Mikah Laufer, & Christopher Wong with additional assistance from
+% Professor John Evans, and the CU ASEN 3802 Faculty team and lab
+% resources.
+
 function [e,c_L,c_Di] = PLLT(b,a0_t,a0_r,c_t,c_r,aero_t,aero_r,geo_t,geo_r,N)
+
 
 i = (1:N);
 %% Create linear
@@ -32,15 +51,14 @@ S = (c_t+c_r)*(b/2);
 % Aspect ratio
 AR = b^2 /S;
 
+
 % Calculate coefficients of lift and drag
 c_L = A_coeffs(1)*pi*AR;
 c_Di = ((c_L^2)*(1+delta))/(pi*AR);
 
 % Calculate span efficiency factor
 e = 1/(1+delta);
-disp("RUNNING NEW PLLT VERSION")
 
 end
-
 
 
